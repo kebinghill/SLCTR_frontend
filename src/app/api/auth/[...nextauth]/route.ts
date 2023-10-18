c27d301a67a0f8a83cb6a3a79f5efd2f81ccdb35
Kevin Gil <93kevingil@gmail.com>
2023-10-18 14:02:50 -0300

    access token to session

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,15 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
 
+const scopes =
+  'scope=user-read-email playlist-modify-public playlist-modify-private';
+
 export const authOptions: NextAuthOptions = {
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID || '',
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET || '',
+      authorization: `${process.env.SPOTIFY_AUTH_URL}?${scopes}`,
       profile(profile) {
         return {
           id: profile.id,
@@ -16,6 +20,18 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
   pages: {
     signIn: '/login',
   },
