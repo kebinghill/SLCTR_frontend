e45b6a9a928b510fe51fbf810aed7525616dc2cb
Kevin Gil <93kevingil@gmail.com>
2023-11-08 12:28:56 -0300

    start swipable playlist image

--- src/app/dashboard/PlaylistImage.tsx
+++ src/app/dashboard/PlaylistImage.tsx
@@ -1,15 +1,21 @@
 'use client';
 
 import Image from 'next/image';
+import { useSwipeable } from 'react-swipeable';
 
 export default function PlaylistImage({ image }: { image: string }) {
+  const handlers = useSwipeable({
+    onSwiped: (eventData) => console.log('User Swiped!', eventData),
+    trackMouse: true,
+  });
   return (
-    <div>
+    <div {...handlers}>
       <Image
         src={image ? image : '/no_image.png'}
         width={600}
         height={600}
         alt='No Image Found'
+        draggable={false}
       />
     </div>
   );
