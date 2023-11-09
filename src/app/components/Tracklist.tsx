b9a70ef87c55b269bb009a3494fb06be65e13c5f
Kevin Gil <93kevingil@gmail.com>
2023-11-09 12:34:06 -0300

    populate tracklist

--- src/app/components/Tracklist.tsx
+++ src/app/components/Tracklist.tsx
@@ -12,124 +12,30 @@ import {
 } from './table';
 
 export default function Tracklist({ tracks }: { tracks: any }) {
-  // remove once api limit cleared
-  const [showTracks, setShowTracks] = useState(false);
+  return (
+    <div className='flex flex-col justify-around  min-w-[500px]'>
+      <Table aria-label='tracklist table' isStriped>
+        <TableHeader>
+          <TableColumn>SONG</TableColumn>
+          <TableColumn>ARTIST</TableColumn>
+          <TableColumn>ALBUM</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {tracks.map((track: any) => {
+            const trackname = track.track.name;
+            const artist = track.track.artists[0].name;
+            const album = track.track.album.name;
 
-  if (showTracks) {
-    return (
-      <div className='flex flex-col justify-around  max-w-[600px]'>
-        <Table aria-label='tracklist table' isStriped>
-          <TableHeader>
-            <TableColumn>SONG</TableColumn>
-            <TableColumn>ARTIST</TableColumn>
-            <TableColumn>ALBUM</TableColumn>
-          </TableHeader>
-          <TableBody>
-            {tracks.map((track: any) => {
-              const trackname = track.track.name;
-              const artist = track.track.artists[0].name;
-              const album = track.track.album.name;
-
-              return (
-                <TableRow key={track.track.id} className='text-black'>
-                  <TableCell>{trackname}</TableCell>
-                  <TableCell>{artist}</TableCell>
-                  <TableCell>{album}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </div>
-    );
-  } else {
-    return (
-      <div className='min-w-[500px] overflow'>
-        <Table aria-label='tracklist table' isStriped>
-          <TableHeader>
-            <TableColumn>SONG</TableColumn>
-            <TableColumn>ARTIST</TableColumn>
-            <TableColumn>ALBUM</TableColumn>
-          </TableHeader>
-          <TableBody>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-            <TableRow className='text-black'>
-              <TableCell>TEST ONE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-              <TableCell>EXAMPLE</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </div>
-    );
-  }
+            return (
+              <TableRow key={track.track.id} className='text-black'>
+                <TableCell>{trackname}</TableCell>
+                <TableCell>{artist}</TableCell>
+                <TableCell>{album}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
--- src/app/dashboard/PlaylistCard.tsx
+++ src/app/dashboard/PlaylistCard.tsx
@@ -11,13 +11,13 @@ export default async function PlaylistCard({
   image: string;
   tracklistHref: string;
 }) {
-  // const { items: tracks } = await getPlaylistTracks(tracklistHref);
+  const { items: tracks } = await getPlaylistTracks(tracklistHref);
 
   return (
     <div className='flex flex-col justify-around min-w-[375px] min-h-[375px] bg-white mx-[30px] mb-[18px]'>
       <Carousel>
         <PlaylistImage image={image} />
-        <Tracklist tracks={null} />
+        <Tracklist tracks={tracks} />
       </Carousel>
       <PlaylistButtons />
     </div>
